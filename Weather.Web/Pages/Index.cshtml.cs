be94dabbc45147cc1ed4dd549b29f1b4d824e222
Suren Mohandass <sumohand@microsoft.com>
2020-06-26 14:41:48 +0100

    Prepared the project for azds and added code to retrieve data from internal api

--- Weather.Web/Pages/Index.cshtml.cs
+++ Weather.Web/Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,18 +10,57 @@ using Microsoft.Extensions.Logging;
 
 namespace Weather.Web.Pages
 {
+
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly IHttpClientFactory _clientFactory;
+
+        public IEnumerable<WeatherForecast> WeatherReport { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory clientFactory)
         {
             _logger = logger;
+            _clientFactory = clientFactory;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
+            var client = _clientFactory.CreateClient();
+            _logger.LogInformation("\n...Retrieving Data...\n");
 
+            var request = new System.Net.Http.HttpRequestMessage();
+            request.RequestUri = new Uri("http://weatherinternalapi/weatherforecast");
+
+            if (this.Request.Headers.ContainsKey("azds-route-as"))
+            {
+                // Propagate the dev space routing header
+                request.Headers.Add("azds-route-as",
+                this.Request.Headers["azds-route-as"] as IEnumerable<string>);
+            }
+            var response = await client.SendAsync(request);
+
+            var jsonResult = await response.Content.ReadAsStringAsync();
+
+            WeatherReport = JsonSerializer.Deserialize<List<WeatherForecast>>(
+                    jsonResult,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            _logger.LogInformation("\n...Finished Retrieving Data...\n");
         }
     }
+
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+
 }
